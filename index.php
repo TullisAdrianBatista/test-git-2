fd8c385927481e6b2e64821574eaac0308ca51ea
TullisAdrianBatist <tullisadrianbatist@gmail.com>
2024-11-03 03:57:51 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
   <h1>hello world</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate eveniet cum sequi magnam ea vitae deserunt sint possimus praesentium odit. Unde odit distinctio tenetur vitae eaque nisi reprehenderit modi quam.</p>
+  <ul>
+    <li>dstu</li>
+    <li>dua</li>
+    <li>tiga</li>
+  </ul>
 </body>
 </html>
